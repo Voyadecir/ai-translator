49a08e6593aa07ea25de1ffe7ccf969f4c960dd0
Voyadecir <admin.voyadecir@proton.me>
2025-11-23 12:27:32 -0600

    Update _init_.py

--- src/python/_init_.py
+++ src/python/_init_.py
@@ -1,7 +1,1 @@
-diff --git a/ai_translator/__init__.py b/ai_translator/__init__.py
-new file mode 100644
-index 0000000000000000000000000000000000000000..cc7fff2758b9acf56f4dd6b2d415dd6a1375ee3a
---- /dev/null
-+++ b/ai_translator/__init__.py
-@@ -0,0 +1 @@
-+"""Voyadecir OCR backend package."""
+
